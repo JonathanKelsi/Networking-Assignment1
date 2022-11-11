2ac4fe1103bba67571d802aafec75bea3443e36f
JonathanKelsi <jonathan.kelsi@gmail.com>
2022-11-12 01:26:21 +0200

    Updated server.py

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 
+'''
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.sendto(b'Hello world', ('127.0.0.1', 12345))
 
@@ -7,3 +8,11 @@ data, addr = s.recvfrom(1024)
 print(str(data), addr)
 
 s.close()
+'''
+
+lst = [1,2,3,4,5]
+
+index = [i for i, v in enumerate(lst) if v == 19]
+
+print(index)
+
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import sys
 import socket
+import sys
 import re
 
 # check input validity
@@ -14,7 +14,7 @@ if len(sys.argv) != 2:
     print(usage_instructions)
     exit(1)
 
-if sys.argv[1] == "--help":
+if sys.argv[1] == '--help':
     print(server_description)
     exit(1)
 
@@ -22,41 +22,90 @@ if not (sys.argv[1].isnumeric() and int(sys.argv[1]) in range(1025, 2 ** 16)):
     print(usage_instructions)
     exit(1)
 
+
 # greet the user
 usr_msg = '    __  __               _ _          _       __     __       ________          __     _____  ____ \n' \
           '   / / / /__  ____ ___  (_| )_____   | |     / /__  / /_     / ____/ /_  ____ _/ /_   / ___/ / __ \\\n' \
-          '  / /_/ / _ \\/ __ `__ \\/ /|// ___/   | | /| / / _ \\/ __ \\   / /   / __ \\/ __ `/ __/  / __ \\ / /_/ /\n' \
+          '  / /_/ / _ \\/ __ `__ \\/ /|// ___/   | | /| / / _ \\/ __ \\   / /   / __ \\/ __ `/ __/  / __ \\ / /_/ /\n'\
           ' / __  /  __/ / / / / / /  (__  )    | |/ |/ /  __/ /_/ /  / /___/ / / / /_/ / /_   / /_/ / \\__, /\n' \
           '/_/ /_/\\___/_/ /_/ /_/_/  /____/     |__/|__/\\___/_.___/   \\____/_/ /_/\\__,_/\\__/   \\____(_)____/\n'
 
 print(usr_msg)
 
+
 # bind a name to the program
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', int(sys.argv[1])))
+
 
 # keep track of the users and the messages
 users = []
 messages = []
 
 
+# utility functions
+def get_user_by_addr(user_addr):
+    user_index = [i for i, u in enumerate(users) if u[1] == user_addr]
+
+    if len(user_index) == 0:
+        return None
+
+    return user_index[0]
+
+
 # functionality
-def join(message, user_addr):
-    name = re.search("1 .*", message)
+def join_chat(user_input, user_addr):
+    name = re.search('1 (.*)', user_input).group(1)
 
     if not name:
         return
 
-    users.insert((name, user_addr, 0))
-
-
-def parse_input(message, user_addr):
-    if len(message) == 0:
+    if get_user_by_addr(user_addr):
         return
 
-    match message[0]:
+    users.append((name, user_addr, 0))
+    messages.append((name, f'{name} has joined'))
+
+
+def send_message(user_input, user_addr):
+    index = get_user_by_addr(user_addr)
+
+    if not index:
+        return
+
+    message = re.search('2 (.*)', user_input).group(1)
+
+    if not message:
+        return
+
+    messages.append((users[index][1], message))
+
+
+def change_name(user_input, user_addr):
+    new_name = re.search('3 (.*)', user_input).group(1)
+
+    if not new_name:
+        return
+
+    index = get_user_by_addr(user_addr)
+
+    if not index:
+        return
+
+    users[index][0] = new_name
+
+
+def parse_input(user_input, user_addr):
+    if len(user_input) == 0:
+        return
+
+    match user_input[0]:
         case '1':
-            join(message, user_addr)
+            join_chat(user_input, user_addr)
+        case '2':
+            send_message(user_input, user_addr)
+        case '3':
+            change_name(user_input, user_addr)
 
 
 while True:
