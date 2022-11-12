dc197faa2e1af0385f29318017b54f26f3abd810
amitmoshcovitz <amitmos2005@gmail.com>
2022-11-12 11:16:29 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,47 +1,14 @@
 import socket
-
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
-# s.sendto(b'Hello world', ('127.0.0.1', 12345))
 
 server_ip = '127.0.0.1'
 server_port = 12345
+default_buffer_size = 1024
 
-# def register(name):
-#     full_output = f'1 {name}'
-#     s.sendto(full_output.encode('ascii') , (server_ip, server_port))
-#
-# def send_message(message):
-#     full_output = f'2 {message}'
-#     s.sendto(full_output.encode('ascii') , (server_ip, server_port))
-
-
-def run_option(option):
-    full_message = ''
-    match option:
-        case '1':
-            name = input("Enter a name:")
-            full_message += f'1 {name}'
-        case '2':
-            message = input("Enter a message:")
-            full_message += f'2 {message}'
-        case '3':
-            name = input("Enter a new name:")
-            full_message += f'3 {name}'
-        case '4':
-            full_message += '4'
-        case '5':
-            full_message += '5'
-
-    s.sendto(full_message.encode('ascii') , (server_ip, server_port))
-
-
-data, addr = s.recvfrom(1024)
-print(str(data), addr)
+while True:
+    message = input()
+    s.sendto(message.encode('ascii') , (server_ip, server_port))
+    data, addr = s.recvfrom(default_buffer_size)
+    print(data)
 
 s.close()
-
-
